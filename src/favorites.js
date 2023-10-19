f9b81704cea4dfa81383371941633877e9ecd678
Ivan Kr <126171248+Dofff95@users.noreply.github.com>
2023-10-19 21:11:38 +0300

    1

--- src/favorites.js
+++ src/favorites.js
@@ -1,10 +1,29 @@
-// Цитата дня - GET https://your-energy.b.goit.study/api/quote
 const quoteURL = "https://your-energy.b.goit.study/api/quote"
 
 const refs = {
-quote: document.querySelector(".quote_text")
+quote: document.querySelector(".quote_text"),
+authorTitle: document.querySelector(".author_name")
 }
-console.log(refs.quote);
-const quoteOfTheDay = async () => {
-    const response = await fetch(quoteURL);
-}
+
+quoteOfTheDay()
+async function quoteOfTheDay() {
+            try {
+                const response = await fetch(quoteURL);
+
+                if (response.ok) {
+                    const data = await response.json();
+                    const quote = data.quote;
+                    const author = data.author;
+
+                    refs.quote.innerHTML = quote;
+                    refs.authorTitle.innerHTML = author;
+                } else {
+                    refs.quote.innerHTML = 'Не вдалося отримати цитату';
+                    authorTitle.innerHTML = '';
+                }
+            } catch (error) {
+                console.error(error);
+                refs.quote.innerHTML = 'Помилка при отриманні цитати';
+                authorTitle.textContent = ''; 
+            }
+        }
