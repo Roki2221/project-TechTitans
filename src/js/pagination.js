d1a0ebb111681bedcc68510cec883b4e50232bcb
anastas786 <agorbatuk08@gmail.com>
2023-10-21 19:55:09 +0200

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -3,14 +3,13 @@ import Pagination from 'tui-pagination';
 const paginationContainer = document.getElementById('tui-pagination-container');
 const exerciseList = document.querySelector('.exercise-card-list');
 
-const itemsPerPage = 5; // Количество элементов на одной странице
-const totalItems = 15; // Общее количество элементов (ваш список упражнений)
-const totalPages = 3; // Количество страниц
-
+const itemsPerPage = 5;
+const totalItems = 15;
+const totalPages = 3;
 const pagination = new Pagination(paginationContainer, {
     totalItems: totalItems,
     itemsPerPage: itemsPerPage,
-    visiblePages: totalPages, // Отображение 3 страниц
+    visiblePages: totalPages,
 });
 
 pagination.on('beforeMove', function (event) {
@@ -22,12 +21,9 @@ function displayExercisePage(page) {
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
 
-    // Отобразите элементы на текущей странице
-    // Например, вставьте элементы из вашего списка упражнений на текущей странице
 }
 
-// Инициализация пагинации
+ии
 pagination.reset(totalItems);
 
-// Первоначальное отображение первой страницы
 displayExercisePage(1);
