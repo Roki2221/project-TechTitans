42c05426e5fe128950ae19c31441f63f8e9034ed
Oleksii <103183090+Vlasvin@users.noreply.github.com>
2023-10-21 17:37:13 +0300

    main

--- src/js/exercisesRightFirstPart.js
+++ src/js/exercisesRightFirstPart.js
@@ -1,62 +1,62 @@
-"use strict";
+'use strict';
 
-import axios from "axios";
+import axios from 'axios';
 
 const refs = {
-    cardList: document.querySelector(".exercise-card-list"),
-    buttonList: document.querySelectorAll(".exercise-section-button"),
-}
+  cardList: document.querySelector('.exercise-card-list'),
+  buttonList: document.querySelectorAll('.exercise-section-button'),
+};
 
-let exerciseMarkup = "";
-let exerciseCardFilterText = "Body parts"
+let exerciseMarkup = '';
+let exerciseCardFilterText = 'Body parts';
 let limit = 12;
 
 if (window.innerWidth < 768) {
-    limit = 9;
+  limit = 9;
 }
 
 refs.buttonList.forEach(button => {
-    button.addEventListener("click", handleClick);
-})
+  button.addEventListener('click', handleClick);
+});
 
 function handleClick(event) {
-    refs.cardList.innerHTML = '';
+  refs.cardList.innerHTML = '';
 
-    const activeButton = document.querySelector(".js-active-filter-button");
-    const currentButton = event.currentTarget;
+  const activeButton = document.querySelector('.js-active-filter-button');
+  const currentButton = event.currentTarget;
 
-    activeButton.disabled = false;
-    activeButton.classList.remove("js-active-filter-button");
-    
-    currentButton.classList.add("js-active-filter-button");
-    currentButton.disabled = true;
+  activeButton.disabled = false;
+  activeButton.classList.remove('js-active-filter-button');
 
-    const query = currentButton.dataset.filter; 
-    
-    markupExerciseCards(query);
+  currentButton.classList.add('js-active-filter-button');
+  currentButton.disabled = true;
+
+  const query = currentButton.dataset.filter;
+
+  markupExerciseCards(query);
 }
 
 async function getExerciseCardsData(query, limit) {
-    try {
-        const endpoint = `https://your-energy.b.goit.study/api/filters?filter=${query}&page=1&limit=${limit}`;
-        const res = await axios.get(endpoint);
-        const data = await res.data;
-        return data;
-    } catch (error) {
-        console.log(error);
-        document.querySelector(".exercise-error").style.display = "block";
-    }
+  try {
+    const endpoint = `https://your-energy.b.goit.study/api/filters?filter=${query}&page=1&limit=${limit}`;
+    const res = await axios.get(endpoint);
+    const data = await res.data;
+    return data;
+  } catch (error) {
+    console.log(error);
+    document.querySelector('.exercise-error').style.display = 'block';
+  }
 }
 
 async function markupExerciseCards(query) {
-    exerciseCardFilterText = query.split("%20").join(' ');
+  exerciseCardFilterText = query.split('%20').join(' ');
 
-    exerciseMarkup = "";
+  exerciseMarkup = '';
 
-    const { results: cards } = await getExerciseCardsData(query, limit);
+  const { results: cards } = await getExerciseCardsData(query, limit);
 
-    cards.forEach(card => {
-        exerciseMarkup += `
+  cards.forEach(card => {
+    exerciseMarkup += `
         <li class="exercise-card-item">
             <img class="exercise-card-img" src="${card.imgURL}" alt="${card.name} card exercises">
             <div class="exercise-card-text-container">
@@ -64,10 +64,10 @@ async function markupExerciseCards(query) {
                 <p class="exercise-card-filter">${exerciseCardFilterText}</p>
             </div>
         </li>
-        `
-    })
+        `;
+  });
 
-    refs.cardList.innerHTML = exerciseMarkup;
+  refs.cardList.innerHTML = exerciseMarkup;
 }
 
-markupExerciseCards("Body%20parts");
+markupExerciseCards('Body%20parts');
--- src/js/exercisesRightSecondPart.js
+++ src/js/exercisesRightSecondPart.js
@@ -1,51 +1,61 @@
+import axios from 'axios';
+
 const refs = {
   cards: document.querySelector('.exercises-cards'),
+  search: document.querySelector('.exercise-input-container'),
 };
+console.log(refs.search);
 
 fetchCards('waist').then(data => {
   console.log(data.results);
   refs.cards.insertAdjacentHTML('beforeend', createMarkupCards(data.results));
 });
 
-function fetchCards(category) {
-  return fetch(
-    `https://your-energy.b.goit.study/api/exercises?bodypart=${category}&page=1&limit=10`
-  ).then(response => {
-    return response.json();
-  });
+async function fetchCards(category) {
+  try {
+    const response = await axios.get(
+      `https://your-energy.b.goit.study/api/exercises?bodypart=${category}&page=1&limit=10`
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 }
+
 function createMarkupCards(arr) {
   return arr
     .map(
       ({ burnedCalories, name, target, rating, bodyPart, time }) => `
       <div class="card">
-      <div class="first-part">
-      <div class="badge">
-      <div class="badge-text">WORKOUT</div>
-      </div>
-    <div class="rating"><p class="rating-text">${rating}</p>
-      <svg class="icon-star" width="18" height="18">
-            <use href="/public/icon.svg#icon-star"></use>
-          </div >
+        <div class="first-part">
+          <div class="badge">
+            <div class="badge-text">WORKOUT</div>
+          </div>
+          <div class="rating">
+            <p class="rating-text">${rating}</p>
+            <svg class="icon-star" width="18" height="18">
+              <use href="/public/icon.svg#icon-star"></use>
+            </svg>
+          </div>
           <button class="start-btn" type="submit">
-              Start
-              <svg class="start-btn-icon" width="16" height="16">
-                <use href="/public/icon.svg#icon-arrow"></use>
-              </svg>
-            </button>
-            </div>
-            <div class="second-part">
-           <svg class="run-man-icon" width="24" height="24">
-                <use href="/public/icon.svg#icon-running-stick-figure-in-cyrcle"></use>
-              </svg> 
-              <p class="exercise-name" >${name}</p>
-              </div>
-              <div class="third-part">
-              <p class="text-card">Burned calories: <span class="value-card">${burnedCalories} / ${time}</span></p>
-              <p class="text-card">Body part: <span class="value-card">${bodyPart}</span></p>
-              <p class="text-card">Target: <span class="value-card">${target}</span></p>
-              </div>
-</div>
+            Start
+            <svg class="start-btn-icon" width="16" height="16">
+              <use href="/public/icon.svg#icon-arrow"></use>
+            </svg>
+          </button>
+        </div>
+        <div class="second-part">
+          <svg class="run-man-icon" width="24" height="24">
+            <use href="/public/icon.svg#icon-running-stick-figure-in-cyrcle"></use>
+          </svg>
+          <p class="exercise-name">${name}</p>
+        </div>
+        <div class="third-part">
+          <p class="text-card">Burned calories: <span class="value-card">${burnedCalories} / ${time}</span></p>
+          <p class="text-card">Body part: <span class="value-card">${bodyPart}</span></p>
+          <p class="text-card">Target: <span class="value-card">${target}</span></p>
+        </div>
+      </div>
     `
     )
     .join('');
