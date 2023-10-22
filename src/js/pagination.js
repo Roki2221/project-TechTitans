dc2e2479098ce198874f1101c2d588b417983fef
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-10-22 09:47:14 +0300

    Update pagination.js

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -7,23 +7,21 @@ const itemsPerPage = 5;
 const totalItems = 15;
 const totalPages = 3;
 const pagination = new Pagination(paginationContainer, {
-    totalItems: totalItems,
-    itemsPerPage: itemsPerPage,
-    visiblePages: totalPages,
+  totalItems: totalItems,
+  itemsPerPage: itemsPerPage,
+  visiblePages: totalPages,
 });
 
 pagination.on('beforeMove', function (event) {
-    const currentPage = event.page;
-    displayExercisePage(currentPage);
+  const currentPage = event.page;
+  displayExercisePage(currentPage);
 });
 
 function displayExercisePage(page) {
-    const startIndex = (page - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
 }
 
-ии
 pagination.reset(totalItems);
 
 displayExercisePage(1);
