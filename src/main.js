a1b4a9d5a4fccd56e06ea1910220cc8c6f980e88
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-10-22 21:32:43 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
 import './js/modal';
-
 import './js/exercisesRightSecondPart';
-
 import './js/exercisesRightFirstPart';
-
-import './js/header';
 import './js/pagination';
-import './js/header'
+import './js/header';
