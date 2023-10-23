6a979263bd66e85fda702e6612e1076e477f8879
anastas786 <agorbatuk08@gmail.com>
2023-10-23 16:23:35 +0200

    upd styles

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,27 +1,41 @@
 import Pagination from 'tui-pagination';
 
+
 const paginationContainer = document.getElementById('tui-pagination-container');
 const exerciseList = document.querySelector('.exercise-card-list');
 
 const itemsPerPage = 5;
 const totalItems = 15;
 const totalPages = 3;
+
 const pagination = new Pagination(paginationContainer, {
+  firstItemClassName: 'first-child',
+  lastItemClassName: 'last-child',
   totalItems: totalItems,
   itemsPerPage: itemsPerPage,
   visiblePages: totalPages,
 });
 
-pagination.on('beforeMove', function (event) {
-  const currentPage = event.page;
-  displayExercisePage(currentPage);
+const sectionButtons = document.querySelectorAll('.exercise-section-button');
+const activeClass = 'js-active-filter-button';
+
+sectionButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    // Удаляем класс активной кнопки у всех кнопок
+    sectionButtons.forEach((btn) => btn.classList.remove(activeClass));
+    // Добавляем класс активной кнопке, которую нажали
+    button.classList.add(activeClass);
+
+    // Здесь можно выполнить действия, связанные с выбранным разделом
+    const selectedSection = button.dataset.filter;
+
+    // Пример: показать или скрыть контент, связанный с разделами
+    if (selectedSection === 'Body%20parts') {
+      // Показать контент для раздела "Body parts"
+    } else if (selectedSection === 'Muscles') {
+      // Показать контент для раздела "Muscles"
+    } else if (selectedSection === 'Equipment') {
+      // Показать контент для раздела "Equipment"
+    }
+  });
 });
-
-function displayExercisePage(page) {
-  const startIndex = (page - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
-}
-
-pagination.reset(totalItems);
-
-displayExercisePage(1);
