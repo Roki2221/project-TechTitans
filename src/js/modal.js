441563090947f218902fed304a05ad83e3ff4227
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-10-25 13:30:01 +0300

    summ

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,7 @@
 const LOCALSTORAGE_KEY = 'exerciseCard';
 let infoData;
 let cardForLS = {};
-let b;
-// ======== перевіряємо наявність даних   =========//
-  
-if (!infoData) {
-  console.log('якщо даних немає');
-  infoData = [];
-}
-const LSData = localStorage.getItem('exerciseCard');
-infoData = JSON.parse(LSData);
-  console.log(infoData);
+
 // ========   =========//
 const btnClose = document.querySelector('.button-close');
 const btnAddFavorites = document.querySelector('.btn-add-favorites');
@@ -25,11 +16,6 @@ btnClose.addEventListener('click', onCloseModal);
 btnAddFavorites.addEventListener('click', addToFavorites);
 window.addEventListener('keydown', closeModal);
 backdrop.addEventListener('click', onCloseModalBackdrop);
-
-
-// fetchParams('64f389465ae26083f39b17a6') //!тут всередині повина бути id
-//   .then(renderModalCard)
-//   .catch(error => console.log(error));
 
 function fetchParams(id) {
   backdrop.classList.remove('is-hidden');
@@ -65,7 +51,37 @@ function addToFavorites(e) {
   // heart.classList.add('add-red');
   writeFormToLS();
 }
-function writeFormToLS(event) {
+// ======== перевіряємо наявність даних   =========//
+
+let LSData;
+LSData = localStorage.getItem(LOCALSTORAGE_KEY);
+
+console.log(LSData);
+// if (!LSData || LSData === undefined) {
+//   infoData = [];
+// }
+// else {
+//   LSData = localStorage.getItem(LOCALSTORAGE_KEY);
+//   infoData = JSON.parse(LSData);
+//   console.log(infoData,'якщо лс щось було');
+// }
+if (LSData) {
+  infoData = JSON.parse(LSData);
+
+  if (infoData.length) {
+    LSData = localStorage.getItem(LOCALSTORAGE_KEY);
+    infoData = JSON.parse(LSData);
+    console.log(infoData, 'якщо лс щось було');
+  }
+  else {
+    infoData = [];
+  }
+}
+else {
+  infoData = [];
+}
+console.log(infoData);
+function writeFormToLS() {
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(infoData));
 }
 
@@ -83,15 +99,6 @@ function createDataCardForLS(data) {
     burnedCalories: `${data.burnedCalories}`,
     description: `${data.description}`,
   };
-  // infoData.map((info) => {
-  //   console.log('ми в мап');
-  //   if (info.id === cardForLS.id) {
-  //     console.log('Ця вправа вже додана в улюблені!');
-  //     return;
-  //   }
-  // });
-  // console.log('схожих немає');
-  // infoData.push(cardForLS); 
   infoData.push(cardForLS);
 }
 // *==================================================*//
@@ -108,27 +115,39 @@ function renderModalCard(data) {
 function createMarkupModal(data) {
   let ratingStar = data.rating.toFixed(1);
   // console.log(data._id);
-  console.log(infoData.length);
-  const saved = localStorage.getItem("exerciseCard");
-  let parsed = JSON.parse(saved);
-  console.log(parsed);
- 
-  let j = 0;
-//  if(infoData.length){}
-for(let item of parsed) {
+  
+  // const saved = localStorage.getItem("exerciseCard");
+  // let parsed = JSON.parse(saved);
+  // console.log(parsed);
+
+  console.log(infoData);
+  for (let item of infoData) {
     console.log(item.id);
+      console.log(data._id);
     if (item.id != data._id) {
-      console.log(j, 'співпадінь немає, кнопка додати');
+      console.log('співпадінь немає, кнопка додати');
       createBtnAdd();
-      
     }
     else {
-      console.log(j, 'у нас є співпадіння, значить кнопка видалити');
+      console.log('у нас є співпадіння, значить кнопка видалити');
       createBtnDelete();
-      break
     }
-    j = +1;
   }
+    // for (let item of infoData) {
+    //   console.log(item.id);
+    //   console.log(data._id);
+    //   if (item.id != data._id) {
+    //     console.log( 'співпадінь немає, кнопка додати');
+    //     createBtnAdd();
+
+    //   }
+    //   else {
+    //     console.log( 'у нас є співпадіння, значить кнопка видалити');
+    //     createBtnDelete();
+    //     return
+    //   }
+    // }
+
   cardModalE.setAttribute('data-id', `${data._id}`);
   return `      
     
@@ -238,7 +257,7 @@ function onClickBtnRemoveFavorites(evt) {
     i = +1;
   })
   // onCloseModal();
-  
+
   //reWrite.createMurkup;
 }
 function createBtnAdd() {
@@ -251,10 +270,9 @@ function createBtnDelete() {
   btnAddFavorites.removeEventListener('click', addToFavorites);
   btnAddFavorites.style.display = 'none';
   btnRemoveFavorites.style.display = '';
-  btnRemoveFavorites.classList.remove('is-hidden');
   btnRemoveFavorites.addEventListener('click', onClickBtnRemoveFavorites);
 }
-        // *==================================================*//
+// *==================================================*//
 
 export {
   onCloseModal,
