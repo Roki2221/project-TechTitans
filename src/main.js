67044f6b672ad61bf63006c9998d7a1fb84ca181
Roki2221 <100676027+Roki2221@users.noreply.github.com>
2023-10-26 15:01:57 +0300

    fix quote of the day

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import './js/modal';
-
 import './js/scrollUp';
-
+import './js/exercisesLeft';
 import './js/exercisesRightSecondPart';
 import './js/exercisesRightFirstPart';
 import './js/pagination';
 import './js/header';
 import './js/footer';
-
